926972116499b5e380ee6e1896d7d95c8e2e755c
Nathaniel <nathaniel.rink@celect.com>
2018-08-16 11:19:13 -0400

    Demo ES6 Modules without a bundler

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
+import utils from './utils.js';
+
 //Axios file sets it up as a global variable
 console.log(axios); //NRHG
+console.log(utils); //NRHG
 
 function performGetRequest1() {
 console.log('GET REQUEST'); //NRHG
-
-	console.log('request 1'); //NRHG
+	var $el1 = $('#getResult1');
+	$el1.innerHTML('');
 }
 
 function performGetRequest2() {
